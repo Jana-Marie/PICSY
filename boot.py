fb51f73613a8b83792516deb08de687c075b9674
Jana <janamarie@janamarie.dev>
2025-01-13 22:20:29 +0100

    updated boot.py

--- boot.py
+++ boot.py
@@ -1,12 +1,48 @@
 import ubuf2png
 import camera
+import machine, os
+import random
+import time
 
-# Inititalize the camera with 8-Bit Grayscale output and a resolution of 400x296px
-camera.init(0, format=camera.GRAYSCALE, fb_location=camera.PSRAM, framesize=camera.FRAME_CIF)
-# Put a frame into buf
-buf = camera.capture()
+class Cam:
+  def __init__(self, fileprefix, shutterpin):
+    print('init cam')
+    self.fileprefix = fileprefix
+    vfs.mount(machine.SDCard(width=4), '/sd')
+    camera.init(0, format=camera.GRAYSCALE, fb_location=camera.PSRAM, framesize=camera.FRAME_CIF)
+    self.shutter = machine.Pin(shutterpin, machine.Pin.IN)
+    
+  def defineShutterButtonIrq(self):
+    self.shutter.irq(handler=self.cam.takePhotoToSD(str(random.randint(1000,9999))), trigger=machine.Pin.IRQ_RISING)
 
-# Initialize our PNG builder
-pngimg = ubuf2png.PngImage(400,296,3,8)
-# Build PNG and print to console
-print(pngimg.buildPng(buf))
+  def pollShutterButton(self):
+    return self.shutter.value()
+
+  def takePhotoToSD(self, filename):
+    _ = camera.capture()
+    buf = camera.capture()
+
+    pngimg = ubuf2png.PngImage(400,296,3,8)
+
+    with open('sd/' + self.fileprefix + filename + '.png', 'wb') as f:
+      f.write(pngimg.buildPng(buf))
+      time.sleep_ms(200)
+      f.close()
+    time.sleep_ms(200)
+
+  def deinit(self):
+    camera.deinit()
+    vfs.unmount('sd/')
+
+if __name__ == '__main__':
+  
+  cam = Cam("awawa-", 33)
+  
+  time.sleep_ms(100)
+  _ = cam.pollShutterButton()
+
+  while True:
+    if cam.pollShutterButton() == 1:
+      cam.takePhotoToSD(str(random.randint(100,999)))
+    else:
+      time.sleep_ms(20)
