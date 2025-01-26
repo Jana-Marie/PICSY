33250c2f5543b8a7477837818fdb97a077580b33
Fiona <git@vidister.de>
2025-01-26 11:51:38 +0000

    Rework file saving

--- boot.py
+++ boot.py
@@ -5,44 +5,68 @@ import random
 import time
 
 class Cam:
-  def __init__(self, fileprefix, shutterpin):
+  def __init__(self, filePrefix, shutterpin):
+    self.fileDir = "DCIM/100PICSY"
+    self.filePrefix = filePrefix
+    print('init filesystem')
+    self.initFilesystem()
+    self.fileIndex = self.getFileIndex()
+
     print('init cam')
-    self.fileprefix = fileprefix
-    vfs.mount(machine.SDCard(width=4), '/sd')
     camera.init(0, format=camera.GRAYSCALE, fb_location=camera.PSRAM, framesize=camera.FRAME_CIF)
     self.shutter = machine.Pin(shutterpin, machine.Pin.IN)
-    
+
+  def initFilesystem(self):
+    vfs.mount(machine.SDCard(width=4), '/sd')
+    path = "/sd"
+    for i in self.fileDir.split("/"):
+        if not i in os.listdir(path):
+            os.mkdir(path + "/" + i)
+        path = path + "/" + i
+
+  def getFileIndex(self):
+      filelist = filter(lambda f: f.startswith(self.filePrefix) and f.endswith(".png"), os.listdir("/sd/" + self.fileDir))
+
+      filelist = sorted(list(filelist))
+      if len(filelist) == 0:
+        return 0
+
+      return int(filelist[-1][len(self.filePrefix):-4]) + 1
+
   def defineShutterButtonIrq(self):
     self.shutter.irq(handler=self.cam.takePhotoToSD(str(random.randint(1000,9999))), trigger=machine.Pin.IRQ_RISING)
 
   def pollShutterButton(self):
     return self.shutter.value()
 
-  def takePhotoToSD(self, filename):
-    _ = camera.capture()
+  def takePhotoToSD(self):
     buf = camera.capture()
+
+    print("Processing photo...")
 
     pngimg = ubuf2png.PngImage(400,296,3,8)
 
-    with open('sd/' + self.fileprefix + filename + '.png', 'wb') as f:
+    with open('/sd/' + self.fileDir + '/' + self.filePrefix + f"{self.fileIndex:04d}" + '.png', 'wb') as f:
       f.write(pngimg.buildPng(buf))
-      time.sleep_ms(200)
       f.close()
-    time.sleep_ms(200)
+
+    # flush filesystem
+    os.sync()
+    print("Done!")
+    self.fileIndex += 1
 
   def deinit(self):
     camera.deinit()
-    vfs.unmount('sd/')
+    vfs.unmount('/sd')
 
 if __name__ == '__main__':
   
-  cam = Cam("awawa-", 33)
-  
+  cam = Cam("PICSY-", 33)
   time.sleep_ms(100)
-  _ = cam.pollShutterButton()
 
   while True:
     if cam.pollShutterButton() == 1:
-      cam.takePhotoToSD(str(random.randint(100,999)))
+      print("Button pressed. Smile!")
+      cam.takePhotoToSD()
     else:
       time.sleep_ms(20)
